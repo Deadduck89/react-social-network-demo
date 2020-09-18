0d1d58cfd515ba4ad1b83ebfdf039da2390a8366
Deadduck89 <70219114+Deadduck89@users.noreply.github.com>
2020-09-18 16:06:28 +0300

    Add project

---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://www.freelogodesign.org/file/app/client/thumb/7f664421-6523-4541-abfc-0db1b6cad04b_200x200.png?1597568443790'/>
+    </header>
+}
+
+export default Header
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png'/>
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            My Posts
+        </div>
+        <div>
+            New Post
+        </div>
+        <div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile
