33d7ac20a0b76f73f66f0dd1f309bfecfcc0d07a
Deadduck89 <70219114+Deadduck89@users.noreply.github.com>
2020-10-02 21:23:46 +0300

    Removed dialogs,messages arrays to index.js

--- src/App.js
+++ src/App.js
@@ -11,18 +11,21 @@ import {BrowserRouter, Route} from "react-router-dom";
 import classes from "./App.module.css"
 
 
-const App = () => {
+const App = (props) => {
     return (
     <BrowserRouter>
         <div className={classes.appWrapper}>
             <Header/>
             <Navbar/>
             <div className={classes.appWrapperContent}>
-                <Route path='/dialogs' component={Dialogs}/>
-                <Route path='/profile' component={Profile}/>
+                {/*<Route path='/dialogs' component={Dialogs}/>
+                <Route path='/profile' component={Profile}/>*/}
                 <Route path='/news' component={News}/>
                 <Route path='/music' component={Music}/>
                 <Route path='/settings' component={Settings}/>
+
+                <Route path='/dialogs' render={ () => <Dialogs dialogs={props.dialogs} messages={props.messages}/>}/>
+                <Route path='/profile' render={ () => <Profile/>}/>
             </div>
         </div>
     </BrowserRouter>
--- src/Components/Dialogs/Dialogs.jsx
+++ src/Components/Dialogs/Dialogs.jsx
@@ -4,24 +4,14 @@ import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Messages/Messages";
 
 const Dialogs = (props) => {
-    let dialogs = [
-        {id: 1, user: 'Andrew'},
-        {id: 2, user: 'Igor'},
-        {id: 3, user: 'Sergey'},
-        {id: 4, user: 'Irina'},
-        {id: 5, user: 'Zhop'}
-    ]
-    let messages = [
-        {id: 1, message: 'Hi!'},
-        {id: 2, message: 'Ti mne bil kak brat!'},
-        {id: 3, message: 'Tut zhop'},
-    ]
-    let dialogElements = dialogs.map(
+    let dialogElements = props.dialogs.map(
         dialog => <DialogItem name={dialog.user} id={dialog.id}/>
     );
-    let messagesElements = messages.map(
+    let messagesElements = props.messages.map(
         message => <Message message={message.message}/>
     );
+
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogItems}>
