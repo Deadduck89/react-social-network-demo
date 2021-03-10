a06915959d5491aee1bfd4ae92aaac19d0715222
Deadduck89 <70219114+Deadduck89@users.noreply.github.com>
2021-03-10 10:02:04 +0300

    stateCopy in dialogs reducer

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -21,15 +21,20 @@ const dialogsReducer = (state = initialState,action) => {
 
     switch(action.type) {
 
-        case SEND_MESSAGE:
+        case SEND_MESSAGE:{
             let body = state.newMessageBody;
-            state.newMessageBody = '';
-            state.messages.push({id: 4, message: body});
-            return state;
+            let stateCopy = {...state};
+            stateCopy.messages = [...state.messages];
+            stateCopy.newMessageBody = '';
+            stateCopy.messages.push({id: 4, message: body});
+            return stateCopy;
+        }
 
-        case UPDATE_NEW_MESSAGE_BODY:
-            state.newMessageBody = action.body;
-            return state;
+        case UPDATE_NEW_MESSAGE_BODY:{
+            let stateCopy = {...state};
+            stateCopy.newMessageBody = action.body;
+            return stateCopy;
+        }
 
         default:
             return state;
