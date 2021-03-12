c0c4b4c528a5dedfa8024e8398c2116f03b6e31a
Deadduck89 <70219114+Deadduck89@users.noreply.github.com>
2021-03-12 16:29:10 +0300

    axios get users

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import styles from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from "../../assets/img/no-avatar.png";
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-            {id: 1, followed: false, name: 'Andrew', avatar: 'https://clck.ru/RaH6Z', status: 'What a nice day!', location: {country: 'Russia', city:'Moscow'}},
-            {id: 2, followed: true, name: 'Igor', avatar: 'https://clck.ru/RQNNi',status: 'Looking for a job', location: {country: 'Russia', city:'Ekaterinburg' }},
-            {id: 3, followed: true, name: 'Sergey', avatar: 'https://clck.ru/RaH7G',status: 'Playing', location: {country: 'Belarus', city:'Minsk'}},
-            {id: 4, followed: false, name: 'Irina', avatar: 'https://clck.ru/RaH87',status: 'Feeling dangerous', location: {country: 'Russia', city:'Moscow'}},
-            {id: 5, followed: true, name: 'Zhop', avatar: 'https://clck.ru/RhWYy',status: 'I am Zhop', location: {country: 'Zhop', city:'Zhopsk'}}
-        ])}
+
+        axios.get( "https://social-network.samuraijs.com/api/1.0/users" ).then( response => {
+
+            props.setUsers( response.data.items )
+        } );
+    }
     return <div>
         {
-            props.users.map(user => <div key={user.id}>
+            props.users.map( user => <div key={user.id}>
                 <span>
                     <div>
-                        <img src={user.avatar} className={styles.userPhoto}/>
+                        <img src={user.photos.small != null ? user.photos.small : userPhoto}
+                             className={styles.userPhoto}/>
                     </div>
                     <div>
                         {user.followed ?
-                            <button onClick={() => {props.unfollow(user.id)}}>Unfollow</button> :
-                            <button onClick={() => {props.follow(user.id)}}>Follow</button>}
+                            <button onClick={() => {
+                                props.unfollow( user.id )
+                            }}>Unfollow</button> :
+                            <button onClick={() => {
+                                props.follow( user.id )
+                            }}>Follow</button>}
                     </div>
                 </span>
                 <span>
@@ -29,11 +35,11 @@ let Users = (props) => {
                         <div>{user.status}</div>
                     </span>
                     <span>
-                        <div>{user.location.country}</div>
-                        <div>{user.location.city}</div>
+                        <div>{"user.location.country"}</div>
+                        <div>{"user.location.city"}</div>
                     </span>
                 </span>
-            </div>)
+            </div> )
         }
     </div>
 
