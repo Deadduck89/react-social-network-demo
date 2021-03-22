bbaf365ddb00539542c5a53a1f179f5442dc7ff7
Deadduck89 <70219114+Deadduck89@users.noreply.github.com>
2021-03-22 08:46:30 +0300

    Changed logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import classes from './Header.module.css'
+import classes from './Header.module.css';
+import logo from '../../assets/logo/Android_O_Preview_Logo.png';
 
 const Header = () => {
     return <header className={classes.header}>
         <img
-            src='https://www.freelogodesign.org/file/app/client/thumb/7f664421-6523-4541-abfc-0db1b6cad04b_200x200.png?1597568443790'/>
+            src={logo}/>
     </header>
 }
 
