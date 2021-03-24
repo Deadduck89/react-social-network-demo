0b390bd31f349a87fb6969979a69145129996a14
Deadduck89 <70219114+Deadduck89@users.noreply.github.com>
2021-03-24 21:26:13 +0300

    Button disabled 1.0

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -28,23 +28,23 @@ let Users = (props) => {
                     </div>
                     <div>
                         {user.followed ?
-                            <button disabled={props.followingInProgress} onClick={() => {
-                                props.toggleFollowingProgress(true);
+                            <button disabled={props.followingInProgress.some(id => id === user.id)} onClick={() => {
+                                props.toggleFollowingProgress(true, user.id);
                                 usersAPI.unfollow(user.id).then( data => {
                                         if (data.resultCode == 0) {
 
                                             props.unfollow( user.id )
                                         }
-                                        props.toggleFollowingProgress(false)
+                                        props.toggleFollowingProgress(false, user.id)
                                     } );
                             }}>Unfollow</button> :
-                            <button disabled={props.followingInProgress} onClick={() => {
-                                props.toggleFollowingProgress(true);
+                            <button disabled={props.followingInProgress.some(id => id === user.id)} onClick={() => {
+                                props.toggleFollowingProgress(true, user.id);
                                 usersAPI.follow(user.id).then( data => {
                                         if (data.resultCode == 0) {
                                             props.follow( user.id )
                                         }
-                                    props.toggleFollowingProgress(false)
+                                    props.toggleFollowingProgress(false, user.id)
                                     } );
 
 
