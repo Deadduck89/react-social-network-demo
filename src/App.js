76460e1cc6d18698ff8e5f4862b69fc1da054116
Deadduck89 <70219114+Deadduck89@users.noreply.github.com>
2021-05-25 16:47:35 +0300

    React.lazy to profile, messages,login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './App.module.css';
 import Navbar from "./Components/Navbar/Navbar";
 import Music from "./Components/Music/Music";
@@ -7,16 +7,17 @@ import Settings from "./Components/Settings/Settings";
 import Friends from "./Components/Friends/Friends"
 import {BrowserRouter, Route, withRouter} from "react-router-dom";
 import classes from "./App.module.css"
-import DialogsContainer from "./Components/Dialogs/DialogsContainer";
 import UsersContainer from "./Components/Users/UsersContainer.jsx";
-import ProfileContainer from "./Components/Profile/ProfileContainer";
 import HeaderContainer from "./Components/Header/HeaderContainer";
-import LoginPage from "./Components/Login/Login";
 import {connect, Provider} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./Components/Common/Preloader";
 import store from "./redux/redux-store";
+
+const DialogsContainer = React.lazy( () => import('.//Components/Dialogs/DialogsContainer') );
+const ProfileContainer = React.lazy( () => import('./Components/Profile/ProfileContainer') );
+const LoginPage = React.lazy( () => import('./Components/Login/Login') );
 
 
 class App extends React.Component {
@@ -37,14 +38,18 @@ class App extends React.Component {
                         <Route path='/news' component={News}/>
                         <Route path='/music' component={Music}/>
                         <Route path='/settings' component={Settings}/>
-                        <Route path='/dialogs'
-                               render={() => <DialogsContainer/>}/>
-                        <Route path='/profile/:userId?'
-                               render={() => <ProfileContainer/>}/>
+                        <Suspense fallback={<Preloader/>}>
+                            <Route path='/dialogs'
+                                   render={() =>
+                                       <DialogsContainer/>}/>
+                            <Route path='/profile/:userId?'
+                                   render={() => <ProfileContainer/>}/>
+                            <Route path='/login' render={() => <LoginPage/>}/>
+                        </Suspense>
                         <Route path='/users'
                                render={() => <UsersContainer/>}/>
                         <Route path='/friends' render={() => <Friends/>}/>
-                        <Route path='/login' render={() => <LoginPage/>}/>
+
                     </div>
                 </div>
             )
