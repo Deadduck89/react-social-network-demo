666395ac6508ae6804c9f9bd8d2d084b8a665f20
Deadduck89 <70219114+Deadduck89@users.noreply.github.com>
2021-06-01 20:20:55 +0300

    Updates

--- precache-manifest.5920e80ba419bae8878f086ce37871cb.js
+++ precache-manifest.5920e80ba419bae8878f086ce37871cb.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2ecd2fc7bed398af055411c307a4f924",
+    "revision": "fb1baca19a53a93b16b62d200d2d914b",
     "url": "/react-social-network-demo/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-social-network-demo/static/css/4.e3baa5d2.chunk.css"
   },
   {
-    "revision": "6776d9410a60ff903147",
+    "revision": "aeae7f6a68312eb2dff3",
     "url": "/react-social-network-demo/static/css/5.23eaffc9.chunk.css"
   },
   {
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-social-network-demo/static/css/6.ef07b5ea.chunk.css"
   },
   {
-    "revision": "a785aebeb966d3f0c23b",
+    "revision": "15b4f045b43068bbf822",
     "url": "/react-social-network-demo/static/css/main.39aabbb4.chunk.css"
   },
   {
@@ -24,32 +24,32 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-social-network-demo/static/js/0.30b5f352.chunk.js"
   },
   {
-    "revision": "b734c2b293fa06229201",
-    "url": "/react-social-network-demo/static/js/3.262bace3.chunk.js"
+    "revision": "fff34e1bcc334dec0d74",
+    "url": "/react-social-network-demo/static/js/3.7f0f9c5e.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/react-social-network-demo/static/js/3.262bace3.chunk.js.LICENSE.txt"
+    "url": "/react-social-network-demo/static/js/3.7f0f9c5e.chunk.js.LICENSE.txt"
   },
   {
     "revision": "4b35401069783a86cc0a",
     "url": "/react-social-network-demo/static/js/4.87cb8192.chunk.js"
   },
   {
-    "revision": "6776d9410a60ff903147",
-    "url": "/react-social-network-demo/static/js/5.e952f735.chunk.js"
+    "revision": "aeae7f6a68312eb2dff3",
+    "url": "/react-social-network-demo/static/js/5.9c3521b1.chunk.js"
   },
   {
     "revision": "4ddbdcac446233a6a8df",
     "url": "/react-social-network-demo/static/js/6.9008afa6.chunk.js"
   },
   {
-    "revision": "a785aebeb966d3f0c23b",
-    "url": "/react-social-network-demo/static/js/main.d610f22f.chunk.js"
+    "revision": "15b4f045b43068bbf822",
+    "url": "/react-social-network-demo/static/js/main.1eaeceee.chunk.js"
   },
   {
-    "revision": "102736e421938834a449",
-    "url": "/react-social-network-demo/static/js/runtime-main.43e487f5.js"
+    "revision": "cd2b7c95863a62d7f828",
+    "url": "/react-social-network-demo/static/js/runtime-main.ce323cd5.js"
   },
   {
     "revision": "525937d7f5756a5665faca0892fba6a1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-social-network-demo/precache-manifest.53878c9324ce7d193dbb75a9d6287701.js"
+  "/react-social-network-demo/precache-manifest.5920e80ba419bae8878f086ce37871cb.js"
 );
 
 self.addEventListener('message', (event) => {
