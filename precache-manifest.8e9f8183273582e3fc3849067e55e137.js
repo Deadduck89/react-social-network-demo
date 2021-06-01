c288aa7650d21bd9659bc77b6994f2720af2243a
Deadduck89 <70219114+Deadduck89@users.noreply.github.com>
2021-06-01 18:34:44 +0300

    Updates

---
+++ precache-manifest.8e9f8183273582e3fc3849067e55e137.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff11f7602534ad70cc140ba684a9079",
+    "url": "/react-social-network-demo/index.html"
+  },
+  {
+    "revision": "92841f37056288f70bf2",
+    "url": "/react-social-network-demo/static/css/4.e3baa5d2.chunk.css"
+  },
+  {
+    "revision": "d8d4b45082dd46c35ed4",
+    "url": "/react-social-network-demo/static/css/5.23eaffc9.chunk.css"
+  },
+  {
+    "revision": "6bfcb36e60c1edb2b6d2",
+    "url": "/react-social-network-demo/static/css/6.ef07b5ea.chunk.css"
+  },
+  {
+    "revision": "df7d77fff2bb63ced863",
+    "url": "/react-social-network-demo/static/css/main.39aabbb4.chunk.css"
+  },
+  {
+    "revision": "b246d3986e6323f1b7c2",
+    "url": "/react-social-network-demo/static/js/0.30b5f352.chunk.js"
+  },
+  {
+    "revision": "b734c2b293fa06229201",
+    "url": "/react-social-network-demo/static/js/3.262bace3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-social-network-demo/static/js/3.262bace3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92841f37056288f70bf2",
+    "url": "/react-social-network-demo/static/js/4.bab66de4.chunk.js"
+  },
+  {
+    "revision": "d8d4b45082dd46c35ed4",
+    "url": "/react-social-network-demo/static/js/5.159510ed.chunk.js"
+  },
+  {
+    "revision": "6bfcb36e60c1edb2b6d2",
+    "url": "/react-social-network-demo/static/js/6.a8132b7f.chunk.js"
+  },
+  {
+    "revision": "df7d77fff2bb63ced863",
+    "url": "/react-social-network-demo/static/js/main.74fa8f08.chunk.js"
+  },
+  {
+    "revision": "638724e74a516b40acc3",
+    "url": "/react-social-network-demo/static/js/runtime-main.b3d5a6cb.js"
+  },
+  {
+    "revision": "525937d7f5756a5665faca0892fba6a1",
+    "url": "/react-social-network-demo/static/media/Android_O_Preview_Logo.525937d7.png"
+  },
+  {
+    "revision": "b42e86e6717698b9ca697e63fab2a665",
+    "url": "/react-social-network-demo/static/media/iconfinder_SocialMedia_Twitter-Outline_2959740.b42e86e6.png"
+  },
+  {
+    "revision": "ce09e10c44f85cf6a0b51e43b18b1cd5",
+    "url": "/react-social-network-demo/static/media/iconfinder_SocialMedia_Website-Outline_2959741.ce09e10c.png"
+  },
+  {
+    "revision": "796ad65da2cc1991f055949789dc7b96",
+    "url": "/react-social-network-demo/static/media/iconfinder_social-79_2285234.796ad65d.png"
+  }
+]);
